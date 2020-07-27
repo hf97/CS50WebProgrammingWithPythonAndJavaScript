16bce473f357041a70ae40ba2c7fb639188b5dbe
HF97 <hugoanf97@gmail.com>
2020-07-27 16:33:49 +0100

    Entry Page a dar

---
+++ p1_wiki/encyclopedia/views.py
@@ -0,0 +1,23 @@
+import markdown2
+
+from django.shortcuts import render
+
+from . import util
+
+def testar(teste):
+    if(teste is not None):
+        return markdown2.markdown(teste)
+    return None
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+
+
+def title(request, title):
+    return render(request, "encyclopedia/entry.html", {
+        "content": testar(util.get_entry(title)),
+        "title": title
+    })
