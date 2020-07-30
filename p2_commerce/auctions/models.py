dd058d6d21bc4b5c29cf710a66d7861e4d021bc8
HF97 <hugoanf97@gmail.com>
2020-07-30 16:06:25 +0100

    Criados models

--- p2_commerce/auctions/models.py
+++ p2_commerce/auctions/models.py
@@ -1,6 +1,28 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from datetime import datetime
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=1000)
+    startingBid = models.FloatField()
+    image = models.URLField()
+    category = models.ForeignKey("Category", on_delete=models.PROTECT)
+    bid = models.ForeignKey("Bid", on_delete=models.PROTECT)
+
+class Bid(models.Model):
+    bidder = models.ForeignKey("User", on_delete=models.PROTECT)
+    price = models.FloatField()
+
+class Comment(models.Model):
+    user = models.ForeignKey("User", on_delete=models.PROTECT)
+    text = models.CharField(max_length=1000)
+    # TODO ver se blank esta bem
+    date = models.DateTimeField(default=datetime.now, blank=True)
